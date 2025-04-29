ac8ec0d04f67b22946dad4abb8b53189c13e6e12
Reginald <reginald2197@gmail.com>
2025-04-29 10:22:10 +0800

    fix: fix trivial issue in login form

--- taph-quiz-app/app/(auth)/login/page.tsx
+++ taph-quiz-app/app/(auth)/login/page.tsx
@@ -4,9 +4,6 @@ import { LoginForm } from '@/components/auth/login-form'
 export default function LoginPage() {
   return (
     <div>
-      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-        Log in to your account
-      </h2>
       <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
       </Suspense>
