8afd23fad45a5eaf5ef23630f13a86e6a14c4512
Reginald <reginald2197@gmail.com>
2025-04-29 10:18:16 +0800

    fix: fix login page - add suspense

--- taph-quiz-app/app/(auth)/login/page.tsx
+++ taph-quiz-app/app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { LoginForm } from '@/components/auth/login-form'
 
 export default function LoginPage() {
@@ -6,7 +7,9 @@ export default function LoginPage() {
       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
         Log in to your account
       </h2>
+      <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
+      </Suspense>
     </div>
   )
 }
